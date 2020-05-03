231367dc6128078f2a5f224f4e9a9c084f38584e
Suphawich <suphawich.skv@gmail.com>
2020-05-03 19:13:12 +0700

    Update Navbar

--- src/components/navbar/NavbarHorizontal.vue
+++ src/components/navbar/NavbarHorizontal.vue
@@ -1,8 +1,7 @@
 <template>
   <b-navbar type="dark" sticky toggleable="md">
     <b-navbar-brand to="/">DOSCG</b-navbar-brand>
-    <b-navbar-toggle target="navbar-toggle-collapse">
-    </b-navbar-toggle>
+    <b-navbar-toggle target="navbar-toggle-collapse"> </b-navbar-toggle>
     <b-collapse id="navbar-toggle-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item to="/DOSCG/function">XYZ</b-nav-item>
@@ -16,21 +15,51 @@
 
 <script>
 export default {
-  name: 'navbar-horizontal',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  name: "navbar-horizontal",
+  data() {
+    return {};
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  .navbar {
-    /*padding-left: 50px;*/
-    /*padding-right: 50px;*/
-    background: #191919 !important;
-    box-shadow: 0 0 10px rgba(0,0,0,.1);
+<style scoped lang="scss">
+.navbar {
+  /*padding-left: 50px;*/
+  /*padding-right: 50px;*/
+  background: #191919 !important;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding-bottom: 0;
+}
+>>> .nav-item {
+  position: relative;
+  &:first-child {
+    margin-left: 0;
   }
+}
+>>> .nav-link {
+  color: #fff !important;
+  font-size: 16px;
+  font-weight: 500;
+  padding: 20px 0;
+  text-decoration: none;
+  display: inline-block;
+  &.router-link-active {
+    color: #f9004d !important;
+    &::after {
+      width: 100%;
+    }
+  }
+  &::after {
+    position: absolute;
+    content: "";
+    left: 0;
+    bottom: 0;
+    width: 0;
+    height: 3px;
+    background: #f9004d;
+    transition: 0.3s;
+    -webkit-transition: 0.3s;
+  }
+}
 </style>
